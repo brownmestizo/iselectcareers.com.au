8f7b94e49907cd52781a7dfe9067a67a10f15006
Raden Sucalit <brownmestizo@gmail.com>
2014-05-22 00:10:26 +1000

    Added phpmailer lib. Added class to form.

--- submit-resume.php
+++ submit-resume.php
@@ -6,6 +6,7 @@
 error_reporting(E_ERROR | E_PARSE);
 //ini_set('display_errors', 1);
 include_once ('libs/form/Zebra_Form.php');
+include_once ('libs/phpmailer/PHPMailerAutoload.php');
 include_once ('model/iselect_modelForm.php');
 
 // instantiate two objects
@@ -86,13 +87,64 @@ $form->clientside_validation(array(
                 'required' => array('error', 'State is required.')
             ));
 
+            // Resume
+            $form->add('label', 'label_fileResume', 'fileResume', 'Upload Resume');
+            $obj = $form->add('file', 'fileResume');
+            $obj->set_rule(array(
+                'required'  =>  array('error', 'PDFs are the best formats for your resumes.'),
+                'upload'    =>  array('tmp', ZEBRA_FORM_UPLOAD_RANDOM_NAMES, 'error', 'Could not upload file!<br>Check that the "tmp" folder exists inside the "examples" folder and that it is writable'),
+                'filetype'  =>  array('pdf', 'error', 'File must be a PDF document.'),
+                'filesize'  =>  array(102400, 'error', 'File size must not exceed 100Kb!'),
+            ));
+
+            // attach a note
+            $form->add('note', 'note_file', 'file', 'File must have the .pdf extension and no more than 100Kb.');
+
+            // Cover Letter
+            $form->add('label', 'label_coverLetter', 'coverLetter', 'Cover Letter');
+            $obj = $form->add('textarea', 'coverLetter');
+            $obj->set_rule(array(
+                'required'  => array('error', 'Cover Letter is required.'),
+                'length'    => array(500, 0, 'error', 'Minimum length is 500 characters!', true),
+            ));
 
             // Submit button
             $form->add('submit', 'btnsubmit', 'Submit');
 
             // Validate the form
             if ($form->validate()) {
-                show_results();
+                /*
+                $mail = new PHPMailer;
+
+                $mail->isSMTP();                                      // Set mailer to use SMTP
+                $mail->Host = 'smtp1.example.com;smtp2.example.com';  // Specify main and backup SMTP servers
+                $mail->SMTPAuth = true;                               // Enable SMTP authentication
+                $mail->Username = 'user@example.com';                 // SMTP username
+                $mail->Password = 'secret';                           // SMTP password
+                $mail->SMTPSecure = 'tls';                            // Enable encryption, 'ssl' also accepted
+
+                $mail->From = 'from@example.com';
+                $mail->FromName = 'Mailer';
+                $mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient
+                $mail->addAddress('ellen@example.com');               // Name is optional
+                $mail->addReplyTo('info@example.com', 'Information');
+
+                $mail->WordWrap = 50;                                 // Set word wrap to 50 characters
+                $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+                $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+                $mail->isHTML(true);                                  // Set email format to HTML
+
+                $mail->Subject = 'Resume Submission';
+                $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+                $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+                if(!$mail->send()) {
+                    echo 'Message could not be sent.';
+                    echo 'Mailer Error: ' . $mail->ErrorInfo;
+                } else {
+                    echo 'Message has been sent';
+                }
+                */
             } else
                 $form->render('templates/submitResume.php');
 
--- templates/submitResume.php
+++ templates/submitResume.php
@@ -43,6 +43,14 @@
                     <div class="cell"><?php echo $label_state . $state; ?></div>
                 </div>
 
+                <div class="row">    
+                    <div class="cell"><?php echo $label_fileResume . $fileResume; ?></div>
+                </div>
+
+                <div class="row even">    
+                    <div class="cell"><?php echo $label_coverLetter . $coverLetter; ?></div>
+                </div>
+
                 <!-- the submit button goes in the last row; also, notice the "last" class which
                 removes the bottom border which is otherwise present for any row -->
                 <div class="row last"><?php echo $btnsubmit?></div>
