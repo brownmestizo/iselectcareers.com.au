6eb24d592e04c8e299f1e2f7e9b7f3c47d28159a
knox-in-sox <emily@rawpixel.com.au>
2014-05-21 17:11:05 +1000

    Em end of day

--- js/jquery.index.js
+++ js/jquery.index.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
   $('#menu li').hover(function() {
       $(this).stop().animate({ paddingBottom: '50px' },500,'easeOutCirc');
   }, function() {
-      $(this).stop().animate({ paddingBottom: '5px' },500,'easeOutBounce');
+      $(this).stop().animate({ paddingBottom: '15px' },500,'easeOutBounce');
   });
 
 });
