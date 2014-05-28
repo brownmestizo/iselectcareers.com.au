b05ddc1f026deb0690af903b4c3296b271e93ea0
unknown <brownmestizo@gmail.com>
2014-05-28 10:48:32 +1000

    Attempt to email results

--- contact-us.php
+++ contact-us.php
@@ -79,6 +79,83 @@
 
     // Validate the form
     if ($form->validate()) {
+        
+          $to = "emily@rawpixel.com.au, brownmestizo@gmail.com";
+          
+          //*** Uniqid Session ***//
+          $strSid = md5(uniqid(time()));
+          
+            $message  = '<html><body>';
+            $message .=  "<p>This email has been sent using the iSelectCareers contact form. </p>";
+            $message .= '<table class="results"><thead><tr><td colspan="2">Submitted values</td></tr></thead>';
+                foreach ($_POST as $key => $value) {
+                    if (strpos($key, 'name_') !== 0 && strpos($key, 'timer_') !== 0 && strpos($key, 'response_') !== 0)
+                        $message .= '<tr><th>' . $key . '</th><td>' . (is_array($value) ? '<pre>' . print_r($value, true) . '</pre>' : $value) . '</td></tr>';
+                }
+            $message .= '</table>';
+            $message .= "</body></html>";
+            
+            //Updated Headers
+            $headers = "From: $fullname <$email> \r\n" . "Reply-To: " . $email . "\r\n" . "X-Mailer: PHP/" . phpversion();
+            $headers .= "MIME-Version: 1.0\r\n";
+            $headers .= "Content-Type: multipart/mixed; boundary=\"".$strSid."\"\n\n";
+            $headers .= "This is a multi-part message in MIME format.\n";   
+            $headers .= "--".$strSid."\n";
+            $headers .= "Content-type: text/html; charset=utf-8\n";
+            $headers .= "Content-Transfer-Encoding: 7bit\n\n";
+            $headers .= $message."\n\n";
+                        
+            //*** Attachment ***//
+            if($_FILES["formfileattachment"]["name"] != "")
+            {
+                $filename = $_FILES["formfileattachment"]["name"];
+                $filecontent = chunk_split(base64_encode(file_get_contents($_FILES["formfileattachment"]["tmp_name"]))); 
+                $headers .= "--".$strSid."\n";
+                $headers .= "Content-Type: application/octet-stream; name=\"".$filename."\"\n"; 
+                $headers .= "Content-Transfer-Encoding: base64\n";
+                $headers .= "Content-Disposition: attachment; filename=\"".$filename."\"\n\n";
+                $headers .= $filecontent."\n\n";
+            }
+            
+            mail($to,$subject,null, $headers);
+            
+            //AUTO REPLY        
+            //$from = 'From:' .$email;  
+            $messagetostudent  = '<html><body>';
+            $messagetostudent .=  "<p>Thank you for your submission.</p>";
+            $messagetostudent .=  "<p>This is an automatic email, please do not reply to this message.</p>";
+            $messagetostudent .=  "<p>We will contact you very soon.</p>";  
+            $messagetostudent .= "</body></html>";
+                
+            $headerstudent = "From: No Reply <noreply@iselectcareers.com.au> \r\n" . "X-Mailer: PHP/" . phpversion();
+            $headerstudent .= "MIME-Version: 1.0\r\n";
+            $headerstudent .= "Content-Type: multipart/mixed; boundary=\"".$strSid."\"\n\n";
+            $headerstudent .= "This is a multi-part message in MIME format.\n"; 
+            $headerstudent .= "--".$strSid."\n";
+            $headerstudent .= "Content-type: text/html; charset=utf-8\n";
+            $headerstudent .= "Content-Transfer-Encoding: 7bit\n\n";
+            $headerstudent .= $messagetostudent."\n\n";
+                        
+            //*** Attachment ***//
+            if($_FILES["formfileattachment"]["name"] != "")
+            {
+                $filename = $_FILES["formfileattachment"]["name"];
+                $filecontent = chunk_split(base64_encode(file_get_contents($_FILES["formfileattachment"]["tmp_name"]))); 
+                $headerstudent .= "--".$strSid."\n";
+                $headerstudent .= "Content-Type: application/octet-stream; name=\"".$filename."\"\n"; 
+                $headerstudent .= "Content-Transfer-Encoding: base64\n";
+                $headerstudent .= "Content-Disposition: attachment; filename=\"".$filename."\"\n\n";
+                $headerstudent .= $filecontent."\n\n";
+            }
+                
+            $studentname = $email;
+            $subjectstudent = 'Thank you for contacting us!';
+                  
+            mail($studentname,$subjectstudent,null, $headerstudent);
+            
+            //END OF AUTO REPLY             
+            
+            $form->render('thankyou.html');         
     } else
         $form->render('templates/contactUs.php');
 
