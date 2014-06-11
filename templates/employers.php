9f5066c88070e71295e94e26b00c1c18f698d36f
Raden Sucalit <brownmestizo@gmail.com>
2014-06-11 21:32:26 +1000

    Fixed employers.php form

--- templates/employers.php
+++ templates/employers.php
@@ -14,6 +14,7 @@
 </head>
 
 <body>
+
 <div id="bodywrap">
   <div id="menu"> <a href="index.html"><img src="images/iselect-logo.png"  alt="iSelectCareers"/></a>
     <ul>
@@ -24,67 +25,82 @@
     <li class="five"><a href="contact-us.php">Contact Us</a></li>
     </ul>
   </div>
+
   <div class="content_left">
- 	<p class="button_green but1"><a href="job-board.html">Job board</a></p>
-    <p class="button_green but2"><a href="submit-resume.php">Submit resume</a></p>
-    <p class="button_green but_highlight"><a href="http://avli.com.au/isc">Apply now</a></p>
-    <p class="button_green but2"><a href="employers.php">Employers</a></p>
-    <p class="button_green but3"><a href="financial-assistance.html">Financial assistance</a></p>
-    <p class="button_green but1"><a href="job-alerts.html">Job alerts</a></p>
-    <p><img src="images/search.png" align="left" /></p>
+     	<p class="button_green but1"><a href="job-board.html">Job board</a></p>
+      <p class="button_green but2"><a href="submit-resume.php">Submit resume</a></p>
+      <p class="button_green but_highlight"><a href="http://avli.com.au/isc">Apply now</a></p>
+      <p class="button_green but2"><a href="employers.php">Employers</a></p>
+      <p class="button_green but3"><a href="financial-assistance.html">Financial assistance</a></p>
+      <p class="button_green but1"><a href="job-alerts.html">Job alerts</a></p>
+      <p><img src="images/search.png" align="left" /></p>
   </div>
+
   <div class="content_wrap join">
-    <div class="content_middle">
-      <h1>Employers</h1>
-      <h2>Register a vacancy or job</h2>
-      <p>Please  use the form to submit your vacancy. </p>
-      <p>We  will actively source potential  candidates through our existing job  seekers  and students </p>
-      <h3>What we will do for you:</h3>
-      <ul>
-        <li>Interview and screen ideal candidates</li>
-        <li>Advertise and promote the job through  our network.</li>
-      </ul>
-      <p>Our focus is to ensure that our  candidates have been upskilled to an Australian National Standard ensuring you  that a candidate for iSelectCareers are the best  choice for your company.</p>
-      <p>A consultants will  be in contact. </p>
-<?php /*?><?php
-                    echo (isset($zf_error) ? $zf_error : (isset($error) ? $error : ''));
-                ?>
-      <div class="row">
-        <div class="cell"><?php echo $label_firstName . $firstName?></div>
-        <div class="cell"><?php echo $label_lastName . $lastName?></div>
+
+      <div class="content_middle">
+        <h1>Employers</h1>
+        <h2>Register a vacancy or job</h2>
+        <p>Please  use the form to submit your vacancy. </p>
+        <p>We  will actively source potential  candidates through our existing job  seekers  and students </p>
+        <h3>What we will do for you:</h3>
+        <ul>
+          <li>Interview and screen ideal candidates</li>
+          <li>Advertise and promote the job through  our network.</li>
+        </ul>
+        <p>Our focus is to ensure that our  candidates have been upskilled to an Australian National Standard ensuring you  that a candidate for iSelectCareers are the best  choice for your company.</p>
+        <p>A consultant will  be in contact. </p>
+
+        <?php echo (isset($zf_error) ? $zf_error : (isset($error) ? $error : '')); ?>
+
+        <div class="row">
+          <div class="cell"><?php echo $label_firstName . $firstName?></div>
+          <div class="cell"><?php echo $label_lastName . $lastName?></div>
+        </div>
+
+        <div class="row even">
+            <div class="cell"><?php echo $label_emailAddress . $emailAddress . $note_emailAddress; ?></div>
+        </div>
+
+        <div class="row">
+            <div class="cell"><?php echo $label_mobileNumber . $mobileNumber . $note_mobileNumber; ?></div>
+        </div>
+
+        <div class="row even">
+            <div class="cell"><?php echo $label_company . $company; ?></div>
+        </div>
+
+        <div class="row">
+            <div class="cell"><?php echo $label_position . $position; ?></div>
+        </div>
+
+        <div class="row even">
+            <div class="cell"><?php echo $label_salary . $salary; ?></div>
+        </div>
+
+        <div class="row ">
+            <div class="cell"><?php echo $label_startdate . $startdate; ?></div>
+        </div>
+
+        <div class="row even">
+            <div class="cell"><?php echo $label_fullpart . $fullpart; ?></div>
+        </div>
+
+        <div class="row">
+            <div class="cell"><?php echo $label_fileResume . $fileResume; ?></div>
+        </div>
+
+        <div class="row last">
+          <div class="cell"><?php echo $btnsubmit?></div>
+        </div>
       </div>
-      <div class="row even">
-        <div class="cell"><?php echo $label_emailAddress . $emailAddress . $note_emailAddress; ?></div>
-      </div>
-      <div class="row">
-        <div class="cell"><?php echo $label_mobileNumber . $mobileNumber . $note_mobileNumber; ?></div>
-      </div>
-      <div class="row even">
-        <div class="cell"><?php echo $label_company . $company; ?></div>
-      </div>
-      <div class="row">
-        <div class="cell"><?php echo $label_position . $position; ?></div>
-      </div>
-      <div class="row even">
-        <div class="cell"><?php echo $label_salary . $salary; ?></div>
-      </div>
-      <div class="row ">
-        <div class="cell"><?php echo $label_startdate . $startdate; ?></div>
-      </div>
-      <div class="row even">
-        <div class="cell"><?php echo $label_fullpart . $fullpart; ?></div>
-      </div>
-      <div class="row">    
-       <div class="cell"><?php echo $label_fileResume . $fileResume; ?></div>
-      </div>
-      
-      <!-- the submit button goes in the last row; also, notice the "last" class which
-                    removes the bottom border which is otherwise present for any row -->
-      <div class="row last"><?php echo $btnsubmit?></div>
-    </div<?php */?>
+
+      <div class="clear clear-bottom"></div>
+
   </div>
-  <div class="clear"></div>
+
 </div>
+
 <div class="footer">
   <p align="center">Copyright 2014 iSelectCareers</p>
 </div>
